496ed21ddae306fc199a6257bea0824d7259fe5f
KyialS <sadybakasova7@gmail.com>
2024-05-03 22:48:37 +0600

    last commit

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -10,14 +10,14 @@ module "vpc" {
 }
 
 module "asg" {
-  source         = "../../modules/autoscaling"
-  public_subnets = [module.vpc.public_subnets_ids["10.0.2.0/24"], module.vpc.private_subnets_ids["10.0.4.0/24"]]
-  sg             = [aws_security_group.sg.id]
-  vpc_id         = module.vpc.vpc_id
-  target_group_arn = module.alb.default_target_group_arn
+  source             = "../../modules/autoscaling"
+  public_subnets     = [module.vpc.public_subnets_ids["10.0.2.0/24"], module.vpc.private_subnets_ids["10.0.4.0/24"]]
+  sg                 = [aws_security_group.sg.id]
+  vpc_id             = module.vpc.vpc_id
+  target_group_arn   = module.alb.default_target_group_arn
   s3_bucket_endpoint = module.s3_bucket.static_website_dns
 
-   
+
 }
 
 resource "aws_security_group" "sg" {
@@ -40,14 +40,21 @@ resource "aws_security_group" "sg" {
 }
 
 module "alb" {
-  source             = "cloudposse/alb/aws"
-  namespace = "alb"
-  subnet_ids = [module.vpc.public_subnets_ids["10.0.2.0/24"], module.vpc.public_subnets_ids["10.0.1.0/24"]]
-  vpc_id     = module.vpc.vpc_id
+  source                           = "cloudposse/alb/aws"
+  namespace                        = "alb"
+  subnet_ids                       = [module.vpc.public_subnets_ids["10.0.2.0/24"], module.vpc.public_subnets_ids["10.0.1.0/24"]]
+  vpc_id                           = module.vpc.vpc_id
+  health_check_path                = "/health"
+  health_check_port                = 80
+  health_check_protocol            = "HTTP"
+  health_check_timeout             = 5
+  health_check_interval            = 30
+  health_check_healthy_threshold   = 2
+  health_check_unhealthy_threshold = 2
 
 }
 
 module "s3_bucket" {
-  source = "../prod"
+  source      = "../prod"
   site_domain = "finalprojectk.com"
 }
